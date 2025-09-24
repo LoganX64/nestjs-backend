986cdb972cd5f61254ec1e0d483ed69336b01c08
LoganX64 <loganxtream@gmail.com>
2025-09-24 16:12:28 +0530

    updated product

--- api-gateway/src/products/products.controller.ts
+++ api-gateway/src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
 import { firstValueFrom } from 'rxjs';
