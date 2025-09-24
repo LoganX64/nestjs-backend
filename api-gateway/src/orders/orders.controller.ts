c59f9357789b86285fa219435167e2a5c223c69f
LoganX64 <loganxtream@gmail.com>
2025-09-24 09:56:06 +0530

    updated orders and product controller

--- api-gateway/src/orders/orders.controller.ts
+++ api-gateway/src/orders/orders.controller.ts
@@ -1,29 +1,39 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { CreateOrderDto } from 'shared/create-order.dto';
 
 @Controller('orders')
+@ApiTags('orders')
 export class OrdersController {
   constructor(private httpService: HttpService) {}
 
-  @Post()
-  create(@Body() orderData: any) {
-    return this.httpService
-      .post('http://localhost:3002/orders', orderData) // Orders Service URL
-      .pipe(map((response) => response.data));
-  }
+  private readonly ordersServiceUrl = process.env.ORDERS_SERVICE_URL!;
 
   @Get()
+  @ApiOperation({ summary: 'Get all orders' })
   findAll() {
     return this.httpService
-      .get('http://localhost:3002/orders')
+      .get(this.ordersServiceUrl) // Orders Service URL
       .pipe(map((response) => response.data));
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get order by ID' })
+  @ApiParam({ name: 'id', type: 'string' })
   findOne(@Param('id') id: string) {
     return this.httpService
-      .get(`http://localhost:3002/orders/${id}`)
+      .get(`${this.ordersServiceUrl}/${id}`)
+      .pipe(map((response) => response.data));
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create an order' })
+  @ApiBody({ type: CreateOrderDto })
+  create(@Body() orderData: any) {
+    return this.httpService
+      .post(this.ordersServiceUrl, orderData) // Orders Service URL
       .pipe(map((response) => response.data));
   }
 }
--- api-gateway/src/products/products.controller.ts
+++ api-gateway/src/products/products.controller.ts
@@ -2,27 +2,35 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
 import { firstValueFrom } from 'rxjs';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { CreateProductDto } from 'shared/create-product.dto';
 
 @Controller('products')
+@ApiTags('products')
 export class ProductsController {
   constructor(private httpService: HttpService) {}
 
-  private readonly productsServiceUrl = 'http://localhost:3001/products';
+  private readonly productsServiceUrl = process.env.PRODUCTS_SERVICE_URL!;
 
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
   findAll() {
     return this.httpService
-      .get('http://localhost:3001/products') // Products Service URL
+      .get(this.productsServiceUrl) // Products Service URL
       .pipe(map((response) => response.data));
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get product by ID' })
+  @ApiParam({ name: 'id', type: 'string' })
   findOne(@Param('id') id: string) {
     return this.httpService
-      .get(`http://localhost:3001/products/${id}`)
+      .get(`${this.productsServiceUrl}/${id}`)
       .pipe(map((response) => response.data));
   }
   @Post()
+  @ApiOperation({ summary: 'Create a product' })
+  @ApiBody({ type: CreateProductDto })
   async createProduct(@Body() body: any) {
     const response = await firstValueFrom(
       this.httpService.post(this.productsServiceUrl, body),
